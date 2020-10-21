2954199063d5dcf968a948b0b68876bc277cab05
rafael <rafael@DESKTOP-3P5M8LL>
2020-10-21 20:16:18 -0300

    atualização

---
+++ src/interfaces/VisitorIF.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+import objetos.Circulo;
+import objetos.Retangulo;
+import objetos.Trapezio;
+import objetos.Triangulo;
+
+public interface VisitorIF {
+	
+	double visitaRetangulo(Retangulo r);
+	double visitaTriangulo(Triangulo t);
+	double visitaCirculo(Circulo c);
+	double visitaTrapezio(Trapezio t);
+}
